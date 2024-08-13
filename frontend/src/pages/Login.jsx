4c59166fb52325daf49cfdfcd59157e11ff28c0d
sakshiii06 <sakshi0807jain@gmail.com>
2024-08-13 20:44:52 +0530

    frontend updated

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
 
     try {
       const body = { email, password };
-      const response = await fetch("http://localhost:3000/login", {
+      const response = await fetch("https://hostel-complaint-system-2-0.onrender.com/login", {
         method: "POST",
         headers: { "content-type": "application/json" },
         body: JSON.stringify(body),
